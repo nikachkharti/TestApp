b99b410f1684a30684a893077556fa07a3adc3f2
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-09-20 15:06:07 +0400

    Project basic structure initialize

---
+++ TaskManager/TaskManager.API/Extensions/ContainerExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace TaskManager.API.Extensions
+{
+    public static class ContainerExtensions
+    {
+        public static void AddControllers(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+        }
+
+        public static void AddSwagger(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Description = "Enter the Bearer Authorization string as following: `Bearer` Generated-JWT-Token",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                });
+
+                options.AddSecurityRequirement(
+                        new OpenApiSecurityRequirement()
+                        {
+                            {
+                                new OpenApiSecurityScheme()
+                                {
+                                    Reference = new OpenApiReference()
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = JwtBearerDefaults.AuthenticationScheme
+                                    }
+                                },
+                                new string[]{}
+                            }
+                        }
+                );
+
+
+                #region XML documentation
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+                #endregion
+
+            });
+        }
+
+    }
+}
---
+++ TaskManager/TaskManager.API/Program.cs
@@ -0,0 +1,24 @@
+using TaskManager.API.Extensions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.AddControllers();
+        builder.AddSwagger();
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        if (builder.Environment.IsDevelopment())
+        {
+            app.UseHttpsRedirection();
+        }
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
