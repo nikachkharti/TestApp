e88d71a53ab7151598eb7155bcc71f94af6b17c3
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-09-20 16:00:02 +0400

    Dto models

---
+++ TaskManager/TaskManager.Application/Models/TodoForGettingDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.Application.Models
+{
+    public record TodoForGettingDto
+    (
+        Guid Id,
+        string Title,
+        DateTime StartDate,
+        DateTime EndDate,
+        bool IsCompleted,
+        Guid UserId
+    );
+}
